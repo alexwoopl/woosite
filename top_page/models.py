f335bdd61a707ed5c2c69934af418e0d29b42f0a
Alex Woo <alexwoopl@gmail.com>
2018-10-24 17:40:03 +1300

    added some paths!

--- top_page/models.py
+++ top_page/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class SiteSection(models.Model):
+    key = models.CharField(max_length=20)
+    url = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- top_page/views.py
+++ top_page/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    return render(request, "top_page/index.html")
+from .models import SiteSection
 
+
+class IndexView(generic.ListView):
+    template_name = "top_page/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['sections'] = [SiteSection(key="game", url="", name="Game Blog"),
+                               SiteSection(key="thoughts", url="", name="Thoughts"),
+                               SiteSection(key="polls", url=reverse('polls:index'), name="Polls"),
+                               SiteSection(key="github", url="", name="Github")]
+        return context
+
+    def get_queryset(self):
+        return
